36ea76c991689b1a953ab07174ade6bb34c5002e
AndreiLucas <andrei.lucas.goncalves@hotmail.com>
2017-11-03 11:21:11 -0200

    Correção 23

---
+++ Códigos/23.cs
@@ -0,0 +1,11 @@
+class Obra {
+    //Atributos e Propriedades
+}
+
+class DisposicaoObra {
+    private string local;
+    public string Local {
+        get { return local; }
+        set { local = value; }
+    }
+}

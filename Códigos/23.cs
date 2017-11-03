4183cca501e2806e6048007d71c9281b6d16bfe9
AndreiLucas <andrei.lucas.goncalves@hotmail.com>
2017-11-03 11:27:21 -0200

    segunda correção na 23

--- Códigos/23.cs
+++ Códigos/23.cs
@@ -3,9 +3,14 @@ class Obra {
 }
 
 class DisposicaoObra {
+    private List<DisposicaoObra> listDisposicao = new List<DisposicaoObra>();
     private string local;
     public string Local {
         get { return local; }
         set { local = value; }
+    }
+
+    public virtual void add(DisposicaoObra d) {
+
     }
 }
